62b4cddb7b8ed1eb9071a0d286bbf444256344d3
Aleksei Mikhalkin <ao.mikhalkin@gmail.com>
2022-07-30 05:12:37 +1000

    create toggle buttons component

--- src/pages/form-elements/form-elements.js
+++ src/pages/form-elements/form-elements.js
@@ -3,4 +3,5 @@ import '@components/ui-header/ui-header';
 import '@components/logo/logo';
 import '@components/text-field/text-field';
 import '@components/radio/radio';
+import '@components/toggle/toggle';
 import './form-elements.scss';
