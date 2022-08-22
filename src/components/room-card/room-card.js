b6e0845a6fc8b471baa2989811e6e9abc397987d
Aleksei Mikhalkin <ao.mikhalkin@gmail.com>
2022-08-23 04:05:31 +1000

    created room-card component

---
+++ src/components/room-card/room-card.js
@@ -0,0 +1,11 @@
+import 'slick-carousel/slick/slick.min';
+
+import '@components/rating/rating';
+
+import './room-card.scss';
+
+$('.room-card__images').slick({
+  accessibility: true,
+  dots: true,
+  arrows: true,
+});
--- src/pages/cards/cards.js
+++ src/pages/cards/cards.js
@@ -2,5 +2,6 @@ import '../../index';
 import '@components/signup-card/signup-card';
 import '@components/signin-card/signin-card';
 import '@components/calendar/calendar';
+import '@components/room-card/room-card';
 
 import './cards.scss';
